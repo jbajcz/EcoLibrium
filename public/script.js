8dfacb71e18a019b1f9641bef1143f1b1ebf196f
jbajcz <bajczjac@msu.edu>
2024-01-28 02:42:53 -0500

    modified:   public/page2.html 	modified:   public/script.js 	modified:   public/style.css

--- public/script.js
+++ public/script.js
@@ -4,3 +4,45 @@ function show() {
         nav.classList.toggle('open');
     }
 }
+
+function buyhouse() {
+    var nav = document.querySelector('.start-game-interface .house');
+    if (nav) {
+        nav.classList.toggle('open');
+    }
+}
+
+function buyMhouse() {
+    var nav = document.querySelector('.start-game-interface .Mhouse');
+    if (nav) {
+        nav.classList.toggle('open');
+    }
+}
+
+function buyapartment() {
+    var nav = document.querySelector('.start-game-interface .apartment');
+    if (nav) {
+        nav.classList.toggle('open');
+    }
+}
+
+function buyfactory() {
+    var nav = document.querySelector('.start-game-interface .factory');
+    if (nav) {
+        nav.classList.toggle('open');
+    }
+}
+
+function buylumber() {
+    var nav = document.querySelector('.start-game-interface .lumbermill');
+    if (nav) {
+        nav.classList.toggle('open');
+    }
+}
+
+function buyfarm() {
+    var nav = document.querySelector('.start-game-interface .farm');
+    if (nav) {
+        nav.classList.toggle('open');
+    }
+}
